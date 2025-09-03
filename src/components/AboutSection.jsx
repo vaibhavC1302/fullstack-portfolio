85e2a3cbe5889f4c18358e72ed9702ad3aa6a2d9
Vaibhav Chauhan <chauhanvaibhav13@gmail.com>
2025-09-03 16:01:26 +0530

    pdf download

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -35,7 +35,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/public/resume.pdf"
+                href="/resume.pdf"
                 download={"Resume.pdf"}
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
